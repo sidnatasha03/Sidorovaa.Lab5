a64a87a34029b900cfa42ad4b59ac2993ad19c9c
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2021-11-09 12:50:49 +0300

    Add files via upload

---
+++ Sidorovaa.Lab5.Exercise2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Sidorova.Lab5.Exercise2
+{
+    class Program
+    {
+        static double Method(double x, double k)
+        {
+
+            double a = 5 * k / Math.Pow(k, 2) + Math.PI * Math.Sqrt(x);
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            Console.WriteLine();
+            double x = Convert.ToDouble(Console.ReadLine());
+            double b = 0.0;
+            Console.WriteLine();
+            Console.WriteLine("Сумма:"); Console.WriteLine();
+            for (int k = 1; k <= 7; k++)
+            {
+                double f = Method(x, k);
+
+                b += f;
+                Console.WriteLine(String.Format("{0:0.0000}   ", b));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Итог: " + b);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Произведение: ");
+            Console.WriteLine();
+            for (int k = 1; k <= 7; k++)
+            {
+                double f = Method(x, k);
+
+                b *= f;
+
+                Console.WriteLine(String.Format("{0:0.0000}   ", b));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Итог: " + b);
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Sidorovaa.Lab5.Exercise3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Sidorova.Lab5.Exercise3;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int cruch = 10;
+        int ckar = 5;
+        int clas = 2;
+        int sum = 100;
+
+        int ruch = 0;
+        int kar = 0;
+        int las = 0;
+
+
+
+        do
+        {
+
+            ruch++;
+
+        }
+        while (ruch < 2);
+
+
+        do
+        {
+
+            kar++;
+
+        }
+        while (kar < 8);
+
+        do
+        {
+
+
+            las++;
+        }
+
+        while (las < 20);
+
+
+        int u = las + ruch + kar;
+
+        if (cruch * ruch + clas * las + ckar * kar == 100)
+        {
+            Console.WriteLine("Ручек: " + ruch);
+            Console.WriteLine("Карандашей: " + kar);
+            Console.WriteLine("Ластиков: " + las);
+            Console.WriteLine("Всего: " + u);
+
+
+        }
+
+        else
+        {
+            Console.WriteLine("error");
+        }
+
+
+    }
+}
+
+
